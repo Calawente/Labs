5e9f1a41e4d8ecc1e3db968d835ab8d5ec8c535e
Calawente <72078382+Calawente@users.noreply.github.com>
2022-08-24 01:24:14 +0300

    Add files via upload

---
+++ Laba3OIB/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Laba3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ключиRSAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.шифрованиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выбратьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.зашифроватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.дешифрованиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выбратьФайлToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.расшифроватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog3 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog3 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog4 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog5 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog6 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ключиRSAToolStripMenuItem,
+            this.шифрованиеToolStripMenuItem,
+            this.дешифрованиеToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // ключиRSAToolStripMenuItem
+            // 
+            this.ключиRSAToolStripMenuItem.Name = "ключиRSAToolStripMenuItem";
+            this.ключиRSAToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.ключиRSAToolStripMenuItem.Text = "Ключи RSA";
+            this.ключиRSAToolStripMenuItem.Click += new System.EventHandler(this.ключиRSAToolStripMenuItem_Click);
+            // 
+            // шифрованиеToolStripMenuItem
+            // 
+            this.шифрованиеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выбратьФайлToolStripMenuItem,
+            this.зашифроватьToolStripMenuItem});
+            this.шифрованиеToolStripMenuItem.Name = "шифрованиеToolStripMenuItem";
+            this.шифрованиеToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.шифрованиеToolStripMenuItem.Text = "Шифрование";
+            // 
+            // выбратьФайлToolStripMenuItem
+            // 
+            this.выбратьФайлToolStripMenuItem.Name = "выбратьФайлToolStripMenuItem";
+            this.выбратьФайлToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.выбратьФайлToolStripMenuItem.Text = "Выбрать файл";
+            this.выбратьФайлToolStripMenuItem.Click += new System.EventHandler(this.выбратьФайлToolStripMenuItem_Click);
+            // 
+            // зашифроватьToolStripMenuItem
+            // 
+            this.зашифроватьToolStripMenuItem.Name = "зашифроватьToolStripMenuItem";
+            this.зашифроватьToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.зашифроватьToolStripMenuItem.Text = "Зашифровать";
+            this.зашифроватьToolStripMenuItem.Click += new System.EventHandler(this.зашифроватьToolStripMenuItem_Click);
+            // 
+            // дешифрованиеToolStripMenuItem
+            // 
+            this.дешифрованиеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выбратьФайлToolStripMenuItem1,
+            this.расшифроватьToolStripMenuItem});
+            this.дешифрованиеToolStripMenuItem.Name = "дешифрованиеToolStripMenuItem";
+            this.дешифрованиеToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.дешифрованиеToolStripMenuItem.Text = "Дешифрование";
+            // 
+            // выбратьФайлToolStripMenuItem1
+            // 
+            this.выбратьФайлToolStripMenuItem1.Name = "выбратьФайлToolStripMenuItem1";
+            this.выбратьФайлToolStripMenuItem1.Size = new System.Drawing.Size(157, 22);
+            this.выбратьФайлToolStripMenuItem1.Text = "Выбрать файл";
+            this.выбратьФайлToolStripMenuItem1.Click += new System.EventHandler(this.выбратьФайлToolStripMenuItem1_Click);
+            // 
+            // расшифроватьToolStripMenuItem
+            // 
+            this.расшифроватьToolStripMenuItem.Name = "расшифроватьToolStripMenuItem";
+            this.расшифроватьToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.расшифроватьToolStripMenuItem.Text = "Расшифровать";
+            this.расшифроватьToolStripMenuItem.Click += new System.EventHandler(this.расшифроватьToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(129, 81);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(191, 276);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(419, 81);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(191, 276);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // saveFileDialog2
+            // 
+            this.saveFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog2_FileOk);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // openFileDialog3
+            // 
+            this.openFileDialog3.FileName = "openFileDialog3";
+            // 
+            // openFileDialog4
+            // 
+            this.openFileDialog4.FileName = "openFileDialog4";
+            this.openFileDialog4.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog4_FileOk);
+            // 
+            // openFileDialog5
+            // 
+            this.openFileDialog5.FileName = "openFileDialog5";
+            this.openFileDialog5.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog5_FileOk);
+            // 
+            // openFileDialog6
+            // 
+            this.openFileDialog6.FileName = "openFileDialog6";
+            this.openFileDialog6.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog6_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ключиRSAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem шифрованиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выбратьФайлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem зашифроватьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem дешифрованиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выбратьФайлToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem расшифроватьToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog6;
+    }
+}
+
