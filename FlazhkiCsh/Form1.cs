bd5f0915e240faee51632486012d77a765c12177
Calawente <72078382+Calawente@users.noreply.github.com>
2022-08-24 01:01:31 +0300

    Add files via upload

---
+++ FlazhkiCsh/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flazhki
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool[] arr = new bool[5] { false, false, false, false, false };
+        bool flag = true;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (arr[1] == false)
+            {
+                arr[1] = true;
+                button2.BackColor = Color.Red;
+            }
+            else
+            {
+                arr[1] = false;
+                button2.BackColor = Color.White;
+            }
+            prov();
+        }
+        private void prov()
+        {
+            flag = true;
+            for(int i = 0; i < 5; i++)
+            {
+                if (arr[i] == false)
+                {
+                    flag = false;
+                }
+            }
+            if (flag == true)
+            {
+                MessageBox.Show("Победа");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (arr[0] == false)
+            {
+                arr[0] = true;
+                button1.BackColor = Color.Red;
+            }
+            else
+            {
+                arr[0] = false;
+                button1.BackColor = Color.White;
+            }
+            if (arr[2] == false)
+            {
+                arr[2] = true;
+                button3.BackColor = Color.Red;
+            }
+            else
+            {
+                arr[2] = false;
+                button3.BackColor = Color.White;
+            }
+            prov();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if (arr[1] == false)
+            {
+                arr[1] = true;
+                button2.BackColor = Color.Red;
+            }
+            else
+            {
+                arr[1] = false;
+                button2.BackColor = Color.White;
+            }
+            if (arr[3] == false)
+            {
+                arr[3] = true;
+                button4.BackColor = Color.Red;
+            }
+            else
+            {
+                arr[3] = false;
+                button4.BackColor = Color.White;
+            }
+            prov();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+            if (arr[2] == false)
+            {
+                arr[2] = true;
+                button3.BackColor = Color.Red;
+            }
+            else
+            {
+                arr[2] = false;
+                button3.BackColor = Color.White;
+            }
+            if (arr[4] == false)
+            {
+                arr[4] = true;
+                button5.BackColor = Color.Red;
+            }
+            else
+            {
+                arr[4] = false;
+                button5.BackColor = Color.White;
+            }
+            prov();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+            if (arr[3] == false)
+            {
+                arr[3] = true;
+                button4.BackColor = Color.Red;
+            }
+            else
+            {
+                arr[3] = false;
+                button4.BackColor = Color.White;
+            }
+            prov();
+        }
+    }
+}
---
+++ generatorCsh/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int n = comboBox1.Items.Count;
+            comboBox1.SelectedIndex = rnd.Next(0, n);
+            n = comboBox2.Items.Count;
+            comboBox2.SelectedIndex = rnd.Next(0, n); 
+            n = comboBox3.Items.Count;
+            comboBox3.SelectedIndex = rnd.Next(0, n); 
+            n = comboBox4.Items.Count;
+            comboBox4.SelectedIndex = rnd.Next(0, n);
+            textBox1.Text = comboBox3.Text + " " + comboBox1.Text + " " + comboBox2.Text + " " + comboBox4.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = comboBox3.Text + " " + comboBox1.Text + " " + comboBox2.Text + " " + comboBox4.Text;
+        }
+    }
+}
