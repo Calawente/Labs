9c97a07a7569ee8a60f9d5fde7f27a97de5bbe51
Calawente <72078382+Calawente@users.noreply.github.com>
2022-08-24 00:53:01 +0300

    Add files via upload

---
+++ ChislaCsh/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chisla
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int flag = 0, ch1, ch2, ch3;
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            ch2 = Convert.ToInt32(textBox2.Text);
+            if (textBox1.Text == "")
+            {
+                textBox3.Text = "Введите оба числа";
+            }
+            else
+            {
+                ch1 = Convert.ToInt32(textBox1.Text);
+                switch (flag)
+                {
+                    case 0:
+                        textBox3.Text = "Выберите действие";
+                        break;
+                    case 1:
+                        ch3 = ch1 + ch2;
+                        textBox3.Text = Convert.ToString(ch3);
+                        break;
+                    case 2:
+                        ch3 = ch1 - ch2;
+                        textBox3.Text = Convert.ToString(ch3);
+                        break;
+                    case 3:
+                        ch3 = ch1 * ch2;
+                        textBox3.Text = Convert.ToString(ch3);
+                        break;
+                    case 4:
+                        if (ch2 != 0)
+                        {
+                            ch3 = ch1 / ch2;
+                            textBox3.Text = Convert.ToString(ch3);
+                        }
+                        else
+                        {
+                            textBox3.Text = "Делить на 0 нельзя";
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = comboBox1.Text;
+            switch (str)
+            {
+                case "Сложить":
+                    label1.Text = "Слагаемое";
+                    label2.Text = "Слагаемое";
+                    label3.Text = "Сумма";
+                    flag = 1;
+                    if (textBox1.Text != "" && textBox2.Text != "")
+                    {
+                        ch3 = ch1 + ch2;
+                        textBox3.Text = Convert.ToString(ch3);
+                    }
+                    break;
+                case "Вычесть":
+                    label1.Text = "Уменьшаемое";
+                    label2.Text = "Вычитаемое";
+                    label3.Text = "Разность";
+                    flag = 2;
+                    if (textBox1.Text != "" && textBox2.Text != "")
+                    {
+                        ch3 = ch1 - ch2;
+                        textBox3.Text = Convert.ToString(ch3);
+                    }
+                    break;
+                case "Умножить":
+                    label1.Text = "Множитель";
+                    label2.Text = "Множитель";
+                    label3.Text = "Произведение";
+                    flag = 3;
+                    if (textBox1.Text != "" && textBox2.Text != "")
+                    {
+                        ch3 = ch1 * ch2;
+                        textBox3.Text = Convert.ToString(ch3);
+                    }
+                    break;
+                case "Разделить":
+                    label1.Text = "Делимое";
+                    label2.Text = "Делитель";
+                    label3.Text = "Частное";
+                    flag = 4;
+                    if (textBox1.Text != "" && textBox2.Text != "")
+                    {
+                        if (ch2 != 0)
+                        {
+                            ch3 = ch1 / ch2;
+                            textBox3.Text = Convert.ToString(ch3);
+                        }
+                        else
+                        {
+                            textBox3.Text = "Делить на 0 нельзя";
+                        }
+                    }
+                    break;
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            ch1 = Convert.ToInt32(textBox1.Text);
+            if (textBox2.Text == "")
+            {
+                textBox3.Text = "Введите оба числа";
+            }
+            else
+            {
+                ch2 = Convert.ToInt32(textBox2.Text);
+                switch (flag)
+                {
+                    case 0:
+                        textBox3.Text = "Выберите действие";
+                        break;
+                    case 1:
+                        ch3 = ch1 + ch2;
+                        textBox3.Text = Convert.ToString(ch3);
+                        break;
+                    case 2:
+                        ch3 = ch1 - ch2;
+                        textBox3.Text = Convert.ToString(ch3);
+                        break;
+                    case 3:
+                        ch3 = ch1 * ch2;
+                        textBox3.Text = Convert.ToString(ch3);
+                        break;
+                    case 4:
+                        if (ch2 != 0)
+                        {
+                            ch3 = ch1 / ch2;
+                            textBox3.Text = Convert.ToString(ch3);
+                        }
+                        else
+                        {
+                            textBox3.Text = "Делить на 0 нельзя";
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+   
+}
---
+++ ColorsCsh/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Colors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar3 = new System.Windows.Forms.HScrollBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Red";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Green";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Blue";
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(145, 36);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(276, 10);
+            this.hScrollBar1.TabIndex = 3;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.Location = new System.Drawing.Point(145, 61);
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(276, 10);
+            this.hScrollBar2.TabIndex = 4;
+            // 
+            // hScrollBar3
+            // 
+            this.hScrollBar3.Location = new System.Drawing.Point(145, 87);
+            this.hScrollBar3.Name = "hScrollBar3";
+            this.hScrollBar3.Size = new System.Drawing.Size(276, 10);
+            this.hScrollBar3.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(437, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(437, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(437, 87);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(164, 140);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(167, 56);
+            this.textBox4.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.hScrollBar3);
+            this.Controls.Add(this.hScrollBar2);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.HScrollBar hScrollBar3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
